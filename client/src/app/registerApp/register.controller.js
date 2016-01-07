eb0b9273988a46a09b9cf02adc6b78dabc4b2967
gaurav gupta <gaurav_bng@hotmail.com>
2016-01-07 16:30:59 +0530

    validation in app registration in UI

--- client/src/app/registerApp/register.controller.js
+++ client/src/app/registerApp/register.controller.js
@@ -1,16 +1,20 @@
 Classified.controller('RegisterController', ['$scope', '$http','$rootScope',  function($scope, $http,$rootScope) {
-
-
     $scope.create = function(){
-        var data = $scope.data;        
-        $http.post("/app", data)
-        .success(function(res){
-            alert("App successfully registered");
-            $scope.data = {};
-        }).error(function(err){
-            alert(err);
-        });     
-        
+        if(!$scope.data) alert("Please fill required field");
+        else{
+            var data = $scope.data;
+            if( !data.appId || !data.apiKey || !data.appName || !data.appVersion ){
+                    alert("Please fill required field");
+            }
+            else{        
+                $http.post("/app", data)
+                .success(function(res){
+                    alert("App successfully registered");
+                    $scope.data = {};
+                }).error(function(err){
+                    alert(err);
+                });   
+            }
+        }
     }
-
 }]);
